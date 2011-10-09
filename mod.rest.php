7f3e3fa7c056f62a6ca5684ea23d6a53f6a00ed6
Mark Huot <mark@markhuot.com>
2011-10-09 05:52:30 -0400

    optimized for better use of api class

--- mod.rest.php
+++ mod.rest.php
@@ -3,25 +3,6 @@
 class Rest
 {
 	
-	public function __construct()
-	{
-		$this->EE =& get_instance();
-	}
 	
-	public function entries()
-	{
-		header('Content-type: application/json');
-		
-		echo json_encode(array(
-			(object)array(
-				'title' => 'test'
-			),
-			(object)array(
-				'title' => 'two'
-			)
-		));
-		
-		exit();
-	}
 	
 }
